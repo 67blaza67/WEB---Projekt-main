8dc734802b14fab2cd8c819b4c3dd4510a684e5b
Matěj Blažíček <majki3626@gmail.com>
2026-05-16 18:05:16 +0200

    extends

--- app.py
+++ app.py
@@ -10,25 +10,26 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     zprava = None
+    tlacitko = False
     if request.method == "POST":
         sifra = request.form.get("sifra")
-        if sifra == "rakev":
+        if sifra.lower() == "rakev":
             zprava = "Správně! Teď něco trochu těžšího. Jak se říká žákovi z doby kamenné? "
-        elif sifra == "pražák":
+        elif sifra.lower() == "pražák":
             zprava = "Výborně! Poslední otázka. Jak se jmenuje stránka s nejlepšími hádankami? "
-        elif sifra == "blážova hádanka":
+        elif sifra.lower() == "blážova hádanka":
             zprava = "Odhalili jste tajemství. Teď přejděte na /tajemstvi kde vám formálně pogratuluji"
+            tlacitko = True
+        elif sifra == None:
+            zprava = "Zadejte odpověď."
+        elif len(sifra) > 30:
+            zprava = "Odpověď je příliš dlouhá. Zkuste to znovu."
         else:
             zprava = "Nesprávná odpověď. Zkuste to znovu."
-    return render_template("index.html", zprava=zprava)
+    return render_template("index.html", zprava=zprava, tlacitko=tlacitko)
 
 
 
-@app.route("/tajemstvi", methods=["GET", "POST"])
-def tajemstvi():
-    if request.method == "POST":
-        tajemstvi = request.form.get("tajemstvi")
-    return render_template("tajemstvi.html")
 
 
 
